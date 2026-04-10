afee3370b47dac8deab60c6d11ed749255e2739b
jlandre72 <jerome.landre@ju.se>
2026-04-10 23:01:12 +0200

    Added course WDF

---
+++ docs/courses/wdf/model-lab4.js
@@ -0,0 +1,23 @@
+// MODEL for projects
+// You MUST use your own projects for your project...
+const projects=[]
+
+// MODEL for skills
+const skills=[
+    {"id":"1", "name":"PHP", "type":"Programming language", "desc":"Programming with PHP on the server side.", "level":4},
+    {"id":"2", "name":"Python", "type":"Programming language", "desc":"Programming with Python.", "level":4},
+    {"id":"3", "name":"Java", "type":"Programming language", "desc":"Programming with Java.", "level":2},
+    {"id":"4", "name":"ImageJ", "type":"Framework", "desc":"Java Framework for Image Processing.", "level":2},
+    {"id":"5", "name":"Javascript", "type":"Programming language", "desc":"Programming with Javascript on the client side.", "level":4},
+    {"id":"6", "name":"Node", "type":"Programming language", "desc":"Programming with Javascript on the server side.", "level":4},
+    {"id":"7", "name":"Express", "type":"Framework", "desc":"A framework for programming Javascript on the server side.", "level":4},
+    {"id":"8", "name":"Scikit-image", "type":"Library", "desc":"A library for Image Processing with Python.", "level":3},
+    {"id":"9", "name":"OpenCV", "type":"Library", "desc":"A library for Image Processing with Python.", "level":4},
+    {"id":"10", "name":"LaTeX", "type":"Description language", "desc":"A language to describe and build professional documents.", "level":5},
+    {"id":"11", "name":"HTML", "type":"Description language", "desc":"A language to create web pages.", "level":4},
+    {"id":"12", "name":"CSS", "type":"Description language", "desc":"A language to apply styles to web pages.", "level":4},
+    {"id":"13", "name":"C", "type":"Programming language", "desc":"The historical language of the Linux/Unix kernels.", "level":3},
+    {"id":"14", "name":"C++", "type":"Programming language", "desc":"A fast high level programming language.", "level":1},
+    {"id":"15", "name":"SQL", "type":"Query language", "desc":"The relational database language to access data (CRUD).", "level":4},
+]
+
